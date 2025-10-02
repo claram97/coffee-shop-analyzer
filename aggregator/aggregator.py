9745a516ced961e274b3ada5b6fed10aecf5a479
claram97 <cruano@fi.uba.ar>
2025-10-02 02:20:48 -0300

    Eliminate comments

--- aggregator/aggregator.py
+++ aggregator/aggregator.py
@@ -8,6 +8,10 @@ processing and analyzing coffee shop data from multiple clients.
 import logging
 
 from aggregator.mock import mock_process
+from aggregator.processing import process_query_2, process_query_3, process_query_4_transactions, serialize_query2_results, serialize_query3_results, serialize_query4_transaction_results
+from aggregator.queryid import QueryId
+from protocol.constants import Opcodes
+from protocol.databatch import DataBatch
 
 
 # from middleware.middleware_client import MessageMiddlewareExchange, MessageMiddlewareQueue
@@ -68,90 +72,90 @@ class Aggregator:
         # self._users_exchange.start_consuming(self._handle_user)
         # logging.debug("Started aggregator server")
         
-        mock_process(self.id)
+        # mock_process(self.id)
 
     def stop(self):
         """Stop the aggregator server."""
         self.running = False
         logging.debug("Stopping aggregator server")
     
-    # def _handle_menu_item(self, message: bytes) -> bool:
-    #     """Process incoming menu item messages."""
-    #     try:
-    #         logging.info(f"Received menu item.  Passing to joiner.")
-    #         self.joiner_queue.send(message)
-    #         return True
-    #     except:
-    #         logging.error(f"Failed to decode menu item message")
-    #         return False
+    def _handle_menu_item(self, message: bytes) -> bool:
+        """Process incoming menu item messages."""
+        try:
+            logging.info(f"Received menu item.  Passing to joiner.")
+            self.joiner_queue.send(message)
+            return True
+        except:
+            logging.error(f"Failed to decode menu item message")
+            return False
     
-    # def _handle_store(self, message: bytes) -> bool:
-    #     """Process incoming store messages."""
-    #     try:
-    #         logging.info(f"Received store. Passing to joiner.")
-    #         self.joiner_queue.send(message)
-    #         return True
-    #     except:
-    #         logging.error(f"Failed to decode store message")
-    #         return False
+    def _handle_store(self, message: bytes) -> bool:
+        """Process incoming store messages."""
+        try:
+            logging.info(f"Received store. Passing to joiner.")
+            self.joiner_queue.send(message)
+            return True
+        except:
+            logging.error(f"Failed to decode store message")
+            return False
     
-    # def _handle_transaction(self, message: bytes):
-    #     """Process incoming transaction messages."""
-    #     try:
-    #         logging.info(f"Received transaction")
-    #         transactions_databatch = DataBatch.deserialize_from_bytes(message)
-    #         transactions = transactions_databatch.batch_msg.rows
-    #         query_id = transactions_databatch.query_ids[0]
-    #         table_id = transactions_databatch.table_ids[0]
-    #         logging.debug(f"Transaction belongs to table {table_id} and query {query_id}")
+    def _handle_transaction(self, message: bytes):
+        """Process incoming transaction messages."""
+        try:
+            logging.info(f"Received transaction")
+            transactions_databatch = DataBatch.deserialize_from_bytes(message)
+            transactions = transactions_databatch.batch_msg.rows
+            query_id = transactions_databatch.query_ids[0]
+            table_id = transactions_databatch.table_ids[0]
+            logging.debug(f"Transaction belongs to table {table_id} and query {query_id}")
 
-    #         if table_id == Opcodes.NEW_TRANSACTION:
-    #             if query_id == QueryId.FIRST_QUERY or query_id == QueryId.EOF:
-    #                 self.joiner_queue.send(message)
-    #             elif query_id == QueryId.THIRD_QUERY:
-    #                 processed_data = process_query_3(transactions)
-    #                 ## Falta devolver el mensaje en formato data batch para poder volver a mandarlo
-    #                 self.joiner_queue.send(message)
-    #             elif query_id == QueryId.FOURTH_QUERY:
-    #                 processed_data = process_query_4_transactions(transactions)
-    #                 ## Falta devolver el mensaje en formato data batch para poder volver a mandarlo
-    #                 self.joiner_queue.send(message)
-    #             else:
-    #                 logging.error(f"Transaction message with unexpected query_id {query_id}")
-    #         else:
-    #             logging.error(f"Transaction message with unexpected table_id {table_id}")        
-    #     except:
-    #         logging.error(f"Failed to decode transaction item message")
+            if table_id == Opcodes.NEW_TRANSACTION:
+                if query_id == QueryId.FIRST_QUERY or query_id == QueryId.EOF:
+                    self.joiner_queue.send(message)
+                elif query_id == QueryId.THIRD_QUERY:
+                    processed_data = process_query_3(transactions)
+                    serialized_data = serialize_query3_results(processed_data)
+                    self.joiner_queue.send(serialized_data)
+                elif query_id == QueryId.FOURTH_QUERY:
+                    processed_data = process_query_4_transactions(transactions)
+                    serialized_data = serialize_query4_transaction_results(processed_data)
+                    self.joiner_queue.send(serialized_data)
+                else:
+                    logging.error(f"Transaction message with unexpected query_id {query_id}")
+            else:
+                logging.error(f"Transaction message with unexpected table_id {table_id}")        
+        except:
+            logging.error(f"Failed to decode transaction item message")
     
-    # def _handle_transaction_item(self, message: bytes):
-    #     """Process incoming transaction item messages."""
-    #     try:
-    #         logging.info(f"Received transaction item")
-    #         transaction_items_databatch = DataBatch.deserialize_from_bytes(message)
-    #         transaction_items = transaction_items_databatch.batch_msg.rows
-    #         query_id = transaction_items_databatch.query_ids[0]
-    #         table_id = transaction_items_databatch.table_ids[0]
+    def _handle_transaction_item(self, message: bytes):
+        """Process incoming transaction item messages."""
+        try:
+            logging.info(f"Received transaction item")
+            transaction_items_databatch = DataBatch.deserialize_from_bytes(message)
+            transaction_items = transaction_items_databatch.batch_msg.rows
+            query_id = transaction_items_databatch.query_ids[0]
+            table_id = transaction_items_databatch.table_ids[0]
             
-    #         if table_id == Opcodes.NEW_TRANSACTION_ITEMS and query_id == QueryId.SECOND_QUERY:
-    #             logging.debug(f"Transaction belongs to table {table_id} and query {query_id}")
-    #             processed_data = process_query_2(transaction_items)
-    #             ## Falta devolver el mensaje en formato data batch para poder volver a mandarlo
-    #             self.joiner_queue.send(message)
-    #         elif table_id == Opcodes.EOF:
-    #             logging.debug(f"EOF message received for transaction items.")
-    #             self.joiner_queue.send(message)
-    #         else:
-    #             logging.error(f"Transaction item message with unexpected table_id {table_id} or query_id {query_id}")            
-    #     except:
-    #         logging.error(f"Failed to decode transaction item message")
+            if table_id == Opcodes.NEW_TRANSACTION_ITEMS and query_id == QueryId.SECOND_QUERY:
+                logging.debug(f"Transaction belongs to table {table_id} and query {query_id}")
+                processed_data = process_query_2(transaction_items)
+                serialized_data = serialize_query2_results(processed_data)
+                self.joiner_queue.send(serialized_data)
+            elif table_id == Opcodes.EOF:
+                logging.debug(f"EOF message received for transaction items.")
+                self.joiner_queue.send(message)
+            else:
+                logging.error(f"Transaction item message with unexpected table_id {table_id} or query_id {query_id}")            
+        except:
+            logging.error(f"Failed to decode transaction item message")
     
-    # def _handle_user(self, message: bytes):
-    #     """Process incoming user messages."""
-    #     try:
-    #         logging.info(f"Received user.  Passing to joiner.")
-    #         self.joiner_queue.send(message)
-    #         return True
-    #     except:
-    #         logging.error(f"Failed to decode user message")
-    #         return False
+    def _handle_user(self, message: bytes):
+        """Process incoming user messages."""
+        try:
+            logging.info(f"Received user.  Passing to joiner.")
+            self.joiner_queue.send(message)
+            return True
+        except:
+            logging.error(f"Failed to decode user message")
+            return False
     
